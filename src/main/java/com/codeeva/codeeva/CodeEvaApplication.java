c1d30a48dec2a129d08919a19c21b737707746ae
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2023-03-24 23:45:12 +0530

    init project

---
+++ src/main/java/com/codeeva/codeeva/CodeEvaApplication.java
@@ -0,0 +1,13 @@
+package com.codeeva.codeeva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeEvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeEvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeeva/codeeva/CodeEvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeeva.codeeva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeEvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
